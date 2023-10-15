41abfc6d5db536ea670db6e709a7daee75b3c938
Nennneko5787 <nennneko@tensi.org>
2023-10-15 15:46:58 +0900

    Pythonしか動かせないサーバーでは動かなくなりました；；

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ from classes import HomuraServerProtocol
 import importlib
 import builtins
 
+from quarry.types.nbt import RegionFile
+
+reactor.suggestThreadPoolSize(50)
+
 HomuraMCVersion = "v0.0.1"
 HomuraMCConfigVersion = "1"
 HomuraMCConfigVersionStr = "v0.0.1"
@@ -125,6 +129,42 @@ if toBool(ini["HomuraMC"]["eula"]) != True:
 	input()
 	sys.exit()
 
+builtins.sent_chunks = False
+builtins.counter = 0
+
+builtins.loaded_regions = {}
+builtins.loaded_chunks = {}
+
+builtins.queue = []
+
+print("Loading spawn chunks, please wait...")
+
+for x in range(-10, 11):
+	for z in range(-10, 11):
+		x2 = x * 16
+		z2 = z * 16
+
+		rx, x2 = divmod(x2, 512)
+		rz, z2 = divmod(z2, 512)
+		cx, x2 = divmod(x2, 16)
+		cz, z2 = divmod(z2, 16)
+
+		if (str(rx) + ";" + str(rz)) in builtins.loaded_regions:
+			region = builtins.loaded_regions[str(rx) + ";" + str(rz)]
+		else:
+			region = RegionFile(os.path.join(os.getcwd(), "assets", "world", "region", "r.%d.%d.mca" % (rx, rz)))
+			builtins.loaded_regions[str(rx) + ";" + str(rz)] = region
+
+		try:
+			if not (str(cx) + ";" + str(cz)) in builtins.loaded_chunks:
+				builtins.loaded_chunks[str(rx) + ";" + str(rz) + "#" + str(cx) + ";" + str(cz)] = region.load_chunk(cx, cz)
+		except ValueError as e:
+			continue
+		except OSError as e:
+			continue
+
+print("Spawn chunks succesfully loaded!")
+
 class HomuraServerFactory(ServerFactory):
 	protocol = HomuraServerProtocol
 	motd = ini["HomuraMC"]["motd"]
@@ -141,7 +181,7 @@ class HomuraServerFactory(ServerFactory):
 		for player in self.players:
 			player.send_packet(
 				"chat_message",
-				player.buff_type.pack_chat(message) + player.buff_type.pack("B", 0),
+				player.buff_type.pack_chat(message) + player.buff_type.pack("B", 0) + player.buff_type.pack_uuid(player.uuid),
 			)
 
 	def send_msg(self, message, name):
@@ -149,7 +189,7 @@ class HomuraServerFactory(ServerFactory):
 			if player.display_name == name:
 				player.send_packet(
 					"chat_message",
-					player.buff_type.pack_chat(message) + player.buff_type.pack("B", 0),
+					player.buff_type.pack_chat(message) + player.buff_type.pack("B", 0) + player.buff_type.pack_uuid(player.uuid),
 				)
 
 	def getPlayers(self):
