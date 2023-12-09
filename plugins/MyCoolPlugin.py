bcebd8acdb04dc006644e4ecbc3d6234712ecb90
nennneko5787 <nennneko5787@gmail.com>
2023-12-09 19:23:46 +0900

    プラグインをわかりやすくした
    ついでにonLoadにしといた

--- plugins/MyCoolPlugin.py
+++ plugins/MyCoolPlugin.py
@@ -2,8 +2,8 @@ from classes import HomuraServerProtocol
 
 class HomuraMCPlugin():
 	@staticmethod
-	def onReady():
-		print("aa")
+	def onLoad():
+		print("Loaded")
 	@staticmethod
 	def onJoinPlayer(self):
-		self.factory.send_msg(f"[Server] Hello There!", self.display_name)
+		self.factory.send_msg(f"[Server] Hello!", self.display_name)
